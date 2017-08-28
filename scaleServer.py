931227b5869a4bcd8e7fb47f667185deaa70d4a5
Cameron Lemmon <cameron.lemmon@sparkfun.com>
2017-08-28 15:57:30 -0600

    Changes things around to be able to use browser native websockets and not need external libs like socket.io

--- scaleServer.py
+++ scaleServer.py
@@ -4,60 +4,72 @@
 
 import json
 import threading
+from geventwebsocket import WebSocketServer, WebSocketApplication, Resource
 
 from readscale import set_scale
 
-from flask import Flask
-from flask import render_template
-from flask_socketio import SocketIO
-
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app, async_mode="threading")
-# socketio = SocketIO(app)
 
 clients = 0
 scale = set_scale()
 
 
-@app.route('/', methods=['GET'])
-def index():
-    scale.update()
-    return render_template('index.html', lbs=scale.pounds, ozs=scale.ounces)
+class WeightApp(WebSocketApplication):
+
+    def on_open(self):
+        print "Connected!"
+        global clients
+        if clients:
+            clients += 1
+            return
+        clients += 1
+        self.send_weight()
+
+    def on_message(self, message, *args, **kwargs):
+        if message:
+            print message
+
+    def on_close(self, reason):
+        global clients
+        clients -= 1
+        print reason
+
+    def send_weight(self, reschedule=0.2):
+        """
+        broadcast the weight on the scale to listeners
+        :param weight: dictionary with
+        :param reschedule: time delay to reschedule the function
+        :return: None
+        """
+        print 'Sending data...'
+        scale.update()
+        weight = {
+            'lbs': scale.pounds,
+            'ozs': scale.ounces
+        }
+        if clients:
+            self.ws.send(json.dumps(weight))
+        if reschedule and clients:
+            threading.Timer(reschedule, self.send_weight).start()
 
 
-def send_weight(reschedule=0.2):
+def static_wsgi_app(environ, start_response):
     """
-    broadcast the weight on the scale to listeners
-    :param weight: dictionary with
-    :param reschedule: time delay to reschedule the function
-    :return: None
+    Serve a test page
+    :param environ:
+    :param start_response:
+    :return:
     """
-    scale.update()
-    weight = {
-        'lbs': scale.pounds,
-        'ozs': scale.ounces
-    }
-    if clients:
-        socketio.send(weight)
-    if reschedule and clients:
-        threading.Timer(reschedule, send_weight).start()
-
-
-@socketio.on('connect')
-def serve_weight():
-    global clients
-    clients += 1
-    send_weight()
-
-
-@socketio.on('disconnect')
-def disconnect():
-    global clients
-    clients -= 1
+    start_response("200 OK", [('Content-Type]', 'text/html')])
+    with open("templates/index.html", 'r') as f:
+        retval = [bytes(line) for line in f.readlines()]
+    return retval
 
 
 if __name__ == '__main__':
     # Default here is 127.0.0.1 and port 5000
-    socketio.run(app)
+    # socketio.run(app)
+    resource = Resource([
+        ('/', static_wsgi_app),
+        ('/data', WeightApp)
+    ])
+    WebSocketServer(('localhost', 8000), resource).serve_forever()
